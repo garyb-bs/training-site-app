05ef2826282000ec7fae201fac7e64f65aca8b00
Gary Behan <gary.b@browserstack.com>
2024-10-09 16:10:13 +0100

    Initial commit

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/")));
 
 app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/html/home.html');
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/html/login.html');
+})
+
+app.get('/login-success', (req, res) => {
+    res.sendFile(__dirname + '/html/success.html');
+})
+
+app.get('/contact', (req, res) => {
     res.sendFile(__dirname + '/html/form.html');
 })
 
